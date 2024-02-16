4a6522f8a88ec51ec85952378802289587524a88
nisanth123 <nisanthmeppayur@gmail.com>
2024-02-16 11:38:00 +0530

    test

--- obj/Release/netcoreapp3.1/WebApplicationMySQL.AssemblyInfo.cs
+++ obj/Release/netcoreapp3.1/WebApplicationMySQL.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationMySQL")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8afc9afc38731aff63c84bc101bf4a19861aef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationMySQL")]
